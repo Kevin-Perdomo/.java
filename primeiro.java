c359676157daa624164200da7769a4eea182dcc0
Kevin-Perdomo <119632064+Kevin-Perdomo@users.noreply.github.com>
2023-01-11 20:19:23 -0300

    Learning

---
+++ primeiro.java
@@ -0,0 +1,6 @@
+public class primeiro {
+    public static void main(String[] args) {
+        System.out.print("\nUma coisa complexa é um amontuado de coisas simples\n");
+        System.out.print("\nUma coisa de cada vez\n\n");
+    }
+}
