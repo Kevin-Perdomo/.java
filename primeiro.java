ba8ec9ff615c1f150d1d941c260eeabb605a74d0
Kevin-Perdomo <119632064+Kevin-Perdomo@users.noreply.github.com>
2023-01-14 11:56:36 -0300

    if, else, switch case; ...

--- primeiro.java
+++ primeiro.java
@@ -1,12 +1,43 @@
 public class primeiro {
     public static void main(String[] args) {
 
-        int numero_1 = 20;
-        int numero_2 = 600;
-        int soma = numero_1 + numero_2;
-        String nome = "Kevin";
+        int nota = 60;
+        int faltas = 20;
+        int tipo_prova = 2;
 
-        System.out.printf("\n\t Soma: %d", soma);
-        System.out.printf("\n\t Nome: %s \n\n", nome);
+        int media = 60;
+        int recuperacao = 40;
+        int max_faltas = 20;
+
+        if ((nota >= media) && (faltas <= max_faltas)) {
+            System.out.println("\n\t Aprovado \n");
+        } else if (nota >= recuperacao && (faltas <= max_faltas)) {
+            System.out.println("\n\t Recuperacao \n");
+        } else {
+            System.out.println("\n\t Reprovado \n");
+        }
+
+        switch (tipo_prova) {
+
+            case 1:
+                System.out.println("\n\t Gabarito 01 \n\n");
+                break;
+
+            case 2:
+                System.out.println("\n\t Gabarito 02 \n\n");
+                break;
+
+            case 3:
+                System.out.println("\n\t Gabarito 03 \n\n");
+                break;
+
+            case 4:
+                System.out.println("\n\t Gabarito 04 \n\n");
+                break;
+
+            default:
+                System.out.println("\n\t Nao tem Gabarito \n\n");
+                break;
+        }
     }
 }
