29b34aacee4a8d32318fc2360056f17765b63a35
Kevin-Perdomo <119632064+Kevin-Perdomo@users.noreply.github.com>
2023-02-09 18:09:29 -0300

    Equação do segundo grau .java

--- Projeto_ 01 - Bhaskara/Equacao_quadratica.java
+++ Projeto_ 01 - Bhaskara/Equacao_quadratica.java
@@ -11,9 +11,9 @@ public class Equacao_quadratica {
         Scanner input = new Scanner(System.in);
         System.out.print("\n\n Informe o valor de a: ");
         a = input.nextDouble();
-        System.out.print("\n\n Informe o valor de b: ");
+        System.out.print("\n Informe o valor de b: ");
         b = input.nextDouble();
-        System.out.print("\n\n Informe o valor de c: ");
+        System.out.print("\n Informe o valor de c: ");
         c = input.nextDouble();
         System.out.printf("\n\n Sua Funcao Quadratica: %.2fX² %.2fX %.2f = 0 \n", a, b, c);
 
@@ -26,7 +26,7 @@ public class Equacao_quadratica {
             System.out.println("\n\n Possui duas raizes reais: \n");
             x1 = ((-b) + Math.sqrt(delta)) / (2 * a);
             x2 = ((-b) - Math.sqrt(delta)) / (2 * a);
-            System.out.printf("Valor da raiz X1 = %.2f", x1);
+            System.out.printf(" Valor da raiz X1 = %.2f", x1);
             System.out.printf("\n\n Valor da raiz X2 = %.2f \nn", x2);
         }
 
@@ -34,12 +34,14 @@ public class Equacao_quadratica {
             System.out.println("\n\n Possui apenas uma raiz real (Multiplicidade de Raizes): \n");
             x1 = ((-b) + Math.sqrt(delta)) / (2 * a);
             x2 = ((-b) - Math.sqrt(delta)) / (2 * a);
-            System.out.printf("Valor da raiz X1 = %.2f", x1);
+            System.out.printf(" Valor da raiz X1 = %.2f", x1);
             System.out.printf("\n\n Valor da raiz X2 = %.2f \n\n", x2);
         }
 
         if (delta < 0) {
             System.out.println("\n\n Nao possui raizes reais \n\n");
         }
+
+        System.exit(0);
     }
 }
