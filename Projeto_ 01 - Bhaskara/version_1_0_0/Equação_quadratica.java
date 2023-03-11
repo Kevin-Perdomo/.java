4e22e40da1923ba8ca49fdebf723a45747083cde
Kevin-Perdomo <119632064+Kevin-Perdomo@users.noreply.github.com>
2023-03-11 20:55:44 -0300

    version_1_0_0

--- Projeto_ 01 - Bhaskara/version_1_0_0/Equação_quadratica.java
+++ Projeto_ 01 - Bhaskara/version_1_0_0/Equação_quadratica.java
@@ -1,5 +1,7 @@
 package version_1_0_0;
 
+import javax.swing.JOptionPane;
+
 public class Equação_quadratica {
 
 	Double a;
@@ -20,6 +22,11 @@ public class Equação_quadratica {
 	public void solve() {
 
 		this.delta = (this.b * this.b) - (4 * this.a * this.c);
+
+		if (delta < 0) {
+			JOptionPane.showMessageDialog(null, "Delta negativo possui raizes complexas");
+		}
+
 		this.x1 = (-this.b + Math.sqrt(this.delta)) / (2 * this.a);
 		this.x2 = (-this.b - Math.sqrt(this.delta)) / (2 * this.a);
 	}
